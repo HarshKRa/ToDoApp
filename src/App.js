0e1048e582855f16087dda99c84001336ca346a7
Harsh Raj Kumar <harsh726181@gmail.com>
2024-06-29 01:45:07 +0530

    final dev submit

--- src/App.js
+++ src/App.js
@@ -65,11 +65,11 @@ function App() {
   }
 
   return (
-    <div className="bg-slate-500 w-[100vw] h-[100vh] py-5">
-      <div className="flex justify-between mx-20">
-        <img src={todoImg} alt="todoImg" />
+    <div className="w-[100vw] mh-[100vh] py-5">
+      <div className="flex flex-col lg:flex-row justify-between lg:mx-20 text-center lg:gap-0 gap-6">
+        <img className="mx-auto lg:mx-0" src={todoImg} alt="todoImg" height="70px" width="70px" />
 
-        <h1 className="my-auto text-white text-2xl font-bold">
+        <h1 className="my-auto text-white lg:text-2xl text-xl font-bold">
           Add Your List Here ✌️
         </h1>
 
@@ -83,41 +83,45 @@ function App() {
           />
           {editData ? (
             <i
-              className="fas fa-plus btnPluse btnPluse input-icon"
+              className="fas fa-plus btnPluse btnPluse input-icon bg-white icon"
               onClick={addData}
               title="Add item"
             ></i>
           ) : (
             <i
-              className="fas fa-solid fa-edit btnPluse input-icon"
+              className="fas fa-solid fa-edit btnPluse input-icon  bg-white icon"
               title="Update item"
               onClick={addData}
             ></i>
            )} 
         </div>
-        <div className="my-auto">
-          <button className="btn" onClick={clearAllData}>
-            Remove All
-          </button>
+        <div className="my-auto lg:block hidden">
+          {
+            data.length > 1 ? (<button className="btn" onClick={clearAllData}>
+              Remove All
+            </button>) : (<button className="btn invisible" onClick={clearAllData}>
+              Remove All
+            </button>) 
+          }
         </div>
       </div>
-      <div className="px-20 py-10 flex flex-wrap gap-10">
+      <div className="lg:px-20 py-10 px-3 flex lg:flex-wrap lg:flex-row flex-col  lg:gap-16 md:8 gap-2 lg:text-xl text-sm">
         {data.map((ele, index) => {
           return (
-            <div className="todo-item">
-              <div className="pr-8">
+            <div className="todo-item bg154242">
+              <div className="pr-8 bg154242">
                 {index + 1}
                 {". "}
                 {ele.name}
               </div>
 
-              <div>
+              <div className="bg154242">
                 <i
-                  className="fas fa-solid fa-edit px-2"
+                  className="fas fa-solid fa-edit px-2 bg154242 icon"
                   onClick={() => handleEdit(ele.id)}
                 ></i>
                 <i
-                  className="fas fa-solid fa-trash"
+                  className="fas fa-solid fa-trash bg154242 icon"
                   onClick={() => removeItem(ele.id)}
                 ></i>
               </div>
@@ -125,6 +129,13 @@ function App() {
           );
         })}
       </div>
+      {
+        data.length >1 ? (<div className="flex justify-center lg:hidden">
+          <button className="btn" onClick={clearAllData}>
+                Remove All
+              </button>
+          </div>) : ""
+      }
     </div>
   );
 }
